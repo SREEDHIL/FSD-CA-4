032e729604ea3d5e08b636fd8e3d0b00b8a2c166
SREEDHIL <sreedhilpavishanker@gmail.com>
2025-04-28 16:10:21 +0530

    Updated

--- Frontend/priority/src/App.jsx
+++ Frontend/priority/src/App.jsx
@@ -10,8 +10,22 @@ const App = () => {
       .catch(error => console.error(error))
   });
 
-  const handleAdd = async(req, res)=>{
-    
+  const handleAdd = async (req, res) => {
+    axios.post("http://localhost:8000/api/task")
+      .then(res => task)
+      .catch(err => console.log(err))
+  }
+
+  const handleEdit = async (req, res) => {
+    axios.put("http://localhost:8000/api/task/:id/priority")
+      .then(res => task)
+      .catch(err => console.log(err))
+  }
+
+  const handleDelete = async (req, res) => {
+    axios.delete("http://localhost:8000/api/task/:id")
+      .then(res => task)
+      .catch(err => console.log(err))
   }
 
   return (
